62d1a5a80711710bdef7b7cde809bcfd9cd1b7a6
Ryan Miller <mill1514@purdue.edu>
2018-04-21 19:46:22 +0000

    Putting the files in there... we will see if it works

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GitPrint
 Print things on your GitHub timeline!
+
+This script will take a string of letters and print them to a 
+GitHub commit timeline by commiting on certain days. 
+
+Needs to be configured heavily.
+
+
---
+++ main.py
@@ -0,0 +1,40 @@
+import letters as l
+import datetime as dt
+
+message = "hire me"
+
+# preprocessing for determining pixel
+today = dt.date.today()
+startdate = dt.date(2018, 4, 21)    # Set this to the sunday you'd like to start on
+                                    # (Github starts on Sunday)
+daysrunning = (today - startdate).days
+
+# Do the junk
+def main():
+    
+    currletterindex = (int) (daysrunning / 49)
+    currweekindex = (int) ( (daysrunning % 49) / 7 )
+    currpixelindex = daysrunning % 7
+
+    #print("Days since start:", daysrunning);
+    #print("Current letter:", message[currletterindex]);
+
+    # Calculate whether or not to 'commit' today
+    alpha = ord(message[currletterindex]) - 96
+    currletter = l.getLetter(alpha)
+    currpixel = currletter[currpixelindex][currweekindex]
+
+    #print("currletter:", alpha, currletter);
+    #print("currweek:", currweekindex, currletter[currpixelindex]);
+    #print("currpixel:", currpixelindex, currpixel);
+
+    if currpixel:
+        print("COMMIT")
+    else:
+        print("DON'T COMMIT")
+
+    return
+
+
+main()
+
