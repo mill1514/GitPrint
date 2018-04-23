cce6025e5e9eae0d4acdc7fb177b4894d2e355f1
Ryan Miller <mill1514@purdue.edu>
2018-04-23 23:29:56 +0000

    GitPrint is in full swing

--- phoneycommit.sh
+++ phoneycommit.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 # Add a random number to a text file
+echo "\n Original Dumbo:\n"
+cat dumbo.txt
 echo $RANDOM > dumbo.txt 
+
+echo "\nNew Dumbo:\n"
+cat dumbo.txt
+
+echo "\nAdding New Dumbo:\n"
 git add *
+
+echo "\nCommit...:\n"
 git commit -m "GitPrint is in full swing"
+
+echo "\nPushing...\n\n"
 git push $(cat ../gitcred.txt)
